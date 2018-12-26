256a9eeb3abe78a2181d4cb31709ebfb4b4c6a95
toastal <toastal@protonmail.com>
2018-12-26 13:48:35 +0700

    upgrade

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,22 @@
-{-# LANGUAGE OverloadedStrings, UnicodeSyntax #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax     #-}
 
 module Main where
 
-import Prelude
+import           Prelude
 
-import Control.Applicative
-import Control.Lens
-import Data.Aeson.Lens (_String, key)
-import Data.Maybe (fromMaybe)
-import Data.Monoid ((<>))
-import qualified Data.List as List
-import qualified Data.Text as Text
-import qualified Data.Text.Format as Format
-import qualified Data.Text.Format.Params as Format
-import qualified Network.Wreq as Wreq
-import qualified Options.Applicative as Optparse
+import           Control.Applicative
+import           Control.Lens
+import           Data.Aeson.Lens          (key, _String)
+import qualified Data.List                as List
+import           Data.Maybe               (fromMaybe)
+import           Data.Monoid              ((<>))
+import qualified Data.Text                as Text
+import qualified Data.Text.Format         as Format
+import qualified Data.Text.Format.Params  as Format
+import qualified Network.Wreq             as Wreq
+import qualified Options.Applicative      as Optparse
 import qualified Options.Applicative.Text as Optparse
 
 
@@ -22,7 +24,7 @@ main ∷ IO ()
 main = do
   let parserOpts = Optparse.info (currencyParser <**> Optparse.helper) Optparse.fullDesc
   ( crypto, currency ) ← Optparse.execParser parserOpts
-  let wreqOpts = Wreq.defaults & Wreq.header "CB-VERSION" .~ [ "2017-11-17" ]
+  let wreqOpts = Wreq.defaults & Wreq.header "CB-VERSION" .~ [ "2018-12-26" ]
   r ← Wreq.getWith wreqOpts $ coinbaseAPI crypto
   let amount = r ^? Wreq.responseBody . key "data" . key "rates" . key currency . _String
   putStrLn . Text.unpack $ fromMaybe ("Error: `" <> currency <> "` not found.") amount
@@ -32,6 +34,7 @@ data Crypto
   = BTC
   | ETH
   | LTC
+  | ZEC
   deriving (Bounded, Enum, Eq, Read, Show)
 
 
